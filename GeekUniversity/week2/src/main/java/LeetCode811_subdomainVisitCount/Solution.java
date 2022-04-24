0e8d702945f4d7a4d05d2a5b95953c0a943c5fad
nico <wubcf@126.com>
2022-04-25 00:12:17 +0800

    solution commit

--- GeekUniversity/week2/src/main/java/LeetCode811_subdomainVisitCount/Solution.java
+++ GeekUniversity/week2/src/main/java/LeetCode811_subdomainVisitCount/Solution.java
@@ -17,6 +17,7 @@ class Solution {
             int idxBlank = cpdomain.indexOf(blank);
             int rep=Integer.valueOf(cpdomain.substring(0,idxBlank));
             int idxDot = idxBlank;
+            //循环解析后面的域名，进行计数
             while(idxDot!=-1){
                 String domain = cpdomain.substring(idxDot+1);
                 Integer domainCount = domainCounts.getOrDefault(domain, 0);
