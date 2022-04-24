26182b5374454acc889ed29feac47b45c8e6b5d0
nico <wubcf@126.com>
2022-04-25 00:11:08 +0800

    solution commit

---
+++ GeekUniversity/week2/src/main/java/LeetCode811_subdomainVisitCount/Solution.java
@@ -0,0 +1,35 @@
+package LeetCode811_subdomainVisitCount;
+
+import java.util.*;
+
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        //出参，最大值
+        List<String> rtn = new ArrayList<>(cpdomains.length*3);
+
+        char dot = '.';
+        char blank = ' ';
+        HashMap<String,Integer> domainCounts = new HashMap<>();
+        for(int i=0,len=cpdomains.length;i<len;i++){
+            //分解cpdomains
+            //找到第一个空格
+            String cpdomain = cpdomains[i];
+            int idxBlank = cpdomain.indexOf(blank);
+            int rep=Integer.valueOf(cpdomain.substring(0,idxBlank));
+            int idxDot = idxBlank;
+            while(idxDot!=-1){
+                String domain = cpdomain.substring(idxDot+1);
+                Integer domainCount = domainCounts.getOrDefault(domain, 0);
+                domainCounts.put(domain,domainCount+rep);
+                idxDot = cpdomain.indexOf(dot,idxDot+1);
+            }
+        }
+        //输出至rtn
+        for(Map.Entry<String,Integer> entry:domainCounts.entrySet()){
+            rtn.add(new StringBuilder().append(entry.getValue()).append(blank).append(entry.getKey()).toString());
+        }
+        //返回出参
+        return rtn;
+
+    }
+}
