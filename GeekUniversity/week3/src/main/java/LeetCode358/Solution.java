f758d82e0d97e1b737746e21770a2ce6d3aa3379
nico <wubcf@126.com>
2022-05-09 00:20:02 +0800

    solution commit

---
+++ GeekUniversity/week3/src/main/java/LeetCode358/Solution.java
@@ -0,0 +1,32 @@
+package LeetCode358;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    //原中序遍历单调递增，反中序遍历单调递减
+    //题目要求node.val(new) = node.val(old)+sum(node.rightchild(new));
+
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if (root!=null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
